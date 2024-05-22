980441e0b27d75ae33e80e395b42e5efb7e27569
TienNguyenKha <tiennguyenkha2001@gmail.com>
2024-05-22 15:23:56 +0700

    fix: update stream processing code

--- data_processing/stream_processing/stream_silver_to_gold.py
+++ data_processing/stream_processing/stream_silver_to_gold.py
@@ -1,4 +1,3 @@
-import os
 import sys
 sys.path.append("../..")
 from utils.helpers import load_cfg
@@ -9,8 +8,8 @@ from pyspark.sql import SparkSession
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml import Pipeline
 from pyspark.ml import PipelineModel
-from pyspark.sql.functions import col, from_json, to_timestamp, udf
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+from pyspark.sql.functions import col, from_json, to_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType
 
 
 if __name__ == "__main__":
@@ -77,73 +76,77 @@ if __name__ == "__main__":
         .option("startingOffsets", "earliest") \
         .load()
     parsed_df = kafka_df.select(from_json(col("value").cast("string"), schema).alias("value")).select("value.*")
-    formatted_df = parsed_df.withColumn("created", to_timestamp(col("created")))\
-                                     .withColumn("datetime", to_timestamp(col("datetime")))\
-                                     .withColumn("user_id_raw", col("user_id"))\
-                                     .withColumn("item_id_raw", col("item_id"))\
-                                    #  .na.drop()
+    formatted_df = parsed_df.withColumn("user_id_raw", col("user_id"))\
+                            .withColumn("item_id_raw", col("item_id"))\
+                            #  .na.drop()
     
     # Rename the columns iteratively (to original name)
     transformed_df = pipeline_model.transform(formatted_df).drop(*cat_cols)
     for column in cat_cols:
         transformed_df = transformed_df.withColumnRenamed(f"{column}_index", column)
 
-    # serialize data:
-    user_df = transformed_df.selectExpr( """to_json(named_struct(
-                                                 'user_shops', user_shops,
-                                                 'user_profile', user_profile,
-                                                 'user_group', user_group,
-                                                 'user_gender', user_gender,
-                                                 'user_age', user_age,
-                                                 'user_consumption_2', user_consumption_2,
-                                                 'user_is_occupied', user_is_occupied,
-                                                 'user_geography', user_geography,
-                                                 'user_intentions', user_intentions,
-                                                 'user_brands', user_brands,
-                                                 'user_categories', user_categories,
-                                                 'user_id_raw', user_id_raw,
-                                                 'user_id', user_id,
-                                                 'created', created,
-                                                 'datetime', datetime
-                                                 )) as value""")
+    changedTypedf = transformed_df.withColumn("user_id_raw", col("user_id_raw").cast(IntegerType()))\
+                                  .withColumn("item_id_raw", col("item_id_raw").cast(IntegerType()))\
+                                  .withColumn("user_id", col("user_id").cast(IntegerType()))\
+                                  .withColumn("item_id", col("item_id").cast(IntegerType()))\
+                                  .withColumn("item_category", col("item_category").cast(IntegerType()))\
+                                  .withColumn("item_shop", col("item_shop").cast(IntegerType()))\
+                                  .withColumn("item_brand", col("item_brand").cast(IntegerType()))\
+                                  .withColumn("user_shops", col("user_shops").cast(IntegerType()))\
+                                  .withColumn("user_profile", col("user_profile").cast(IntegerType()))\
+                                  .withColumn("user_group", col("user_group").cast(IntegerType()))\
+                                  .withColumn("user_gender", col("user_gender").cast(IntegerType()))\
+                                  .withColumn("user_age", col("user_age").cast(IntegerType()))\
+                                  .withColumn("user_consumption_2", col("user_consumption_2").cast(IntegerType()))\
+                                  .withColumn("user_is_occupied", col("user_is_occupied").cast(IntegerType()))\
+                                  .withColumn("user_geography", col("user_geography").cast(IntegerType()))\
+                                  .withColumn("user_intentions", col("user_intentions").cast(IntegerType()))\
+                                  .withColumn("user_brands", col("user_brands").cast(IntegerType()))\
+                                  .withColumn("user_categories", col("user_categories").cast(IntegerType()))\
+                                  .withColumn("click", col("click").cast(IntegerType()))\
+                                  .withColumn("created", to_timestamp(col("created")))\
+                                  .withColumn("datetime", to_timestamp(col("datetime")))\
 
-    item_df = transformed_df.selectExpr( """to_json(named_struct(
-                                                 'item_category', item_category,
-                                                 'item_shop', item_shop,
-                                                 'item_brand', item_brand,
-                                                 'item_id_raw', item_id_raw,
-                                                 'item_id', item_id,
-                                                 'created', created,
-                                                 'datetime', datetime
-                                                 )) as value""")
+    #serialize data    
+    alicpp_df = changedTypedf.selectExpr( """to_json(named_struct(
+                                            'user_id_raw', user_id_raw,
+                                            'item_id_raw', item_id_raw,
+                                            'user_id', user_id,
+                                            'item_id', item_id,
+                                            'item_category', item_category,
+                                            'item_shop', item_shop,
+                                            'item_brand', item_brand,
+                                            'user_shops', user_shops,
+                                            'user_profile', user_profile,
+                                            'user_group', user_group,
+                                            'user_gender', user_gender,
+                                            'user_age', user_age,
+                                            'user_consumption_2', user_consumption_2,
+                                            'user_is_occupied', user_is_occupied,
+                                            'user_geography', user_geography,
+                                            'user_intentions', user_intentions,
+                                            'user_brands', user_brands,
+                                            'user_categories', user_categories,
+                                            'click', click,
+                                            'created', created,
+                                            'datetime', datetime
+                                           )) as value""")
 
-    userfeatures_writer_query = user_df \
+    alicpp_df_writer_query = alicpp_df \
         .writeStream \
-        .queryName("userfeatures Writer") \
+        .queryName("alicpp_df Writer") \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "localhost:9092") \
-        .option("topic", "user_df_stream_data") \
+        .option("topic", "alicpp_stream_data") \
         .outputMode("append") \
         .trigger(processingTime="10 second") \
         .partitionBy("user_group")\
-        .option("checkpointLocation", "chk-point-dir/userfeatures") \
+        .option("checkpointLocation", "chk-point-dir/alicpp_df") \
         .start()
     
-    itemfeatures_writer_query = item_df \
-        .writeStream \
-        .queryName("itemfeatures Writer") \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:9092") \
-        .option("topic", "item_df_stream_data") \
-        .outputMode("append") \
-        .trigger(processingTime="10 second") \
-        .partitionBy("item_category")\
-        .option("checkpointLocation", "chk-point-dir/itemfeatures") \
-        .start()
     
     logger.info("Listening and writing to Kafka")
-    userfeatures_writer_query.awaitTermination()
-    itemfeatures_writer_query.awaitTermination()
+    alicpp_df_writer_query.awaitTermination()
 
     
    
