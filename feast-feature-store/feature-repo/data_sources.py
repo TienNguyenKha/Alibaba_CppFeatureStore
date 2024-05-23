7b35f4fac65f74301cb92f413863fb2b0214c46a
TienNguyenKha <tiennguyenkha2001@gmail.com>
2024-05-23 14:41:23 +0700

    fix: update datasource feast

--- feast-feature-store/feature-repo/data_sources.py
+++ feast-feature-store/feature-repo/data_sources.py
@@ -22,28 +22,7 @@ alicpp_stats_stream_source = KafkaSource(
     timestamp_field="created",
     batch_source=alicpp_stats_batch_source,
     message_format=JsonFormat(
-        schema_json="user_id_raw integer,\
-                     item_id_raw integer,\
-                     user_id integer,\
-                     item_id integer,\
-                     item_category integer,\
-                     item_shop integer,\
-                     item_brand integer,\
-                     user_shops integer,\
-                     user_profile integer,\
-                     user_group integer,\
-                     user_gender integer,\
-                     user_age integer,\
-                     user_consumption_2 integer,\
-                     user_is_occupied integer,\
-                     user_geography integer,\
-                     user_intentions integer,\
-                     user_brands integer,\
-                     user_categories integer,\
-                     click integer,\
-                     created timestamp,\
-                     datetime timestamp,\
-                     "
+        schema_json="user_id_raw integer, item_id_raw integer, user_id integer, item_id integer, item_category integer, item_shop integer, item_brand integer, user_shops integer, user_profile integer, user_group integer, user_gender integer, user_age integer, user_consumption_2 integer, user_is_occupied integer, user_geography integer, user_intentions integer, user_brands integer, user_categories integer, click integer, created timestamp, datetime timestamp"
     ),
     watermark_delay_threshold=timedelta(minutes=1),
 )
